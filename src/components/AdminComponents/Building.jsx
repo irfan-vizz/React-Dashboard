513ab171d699f58511e77429fed48dbeb28bd1bc
irfan-vizz <engr.irfanali970@gmail.com>
2023-10-28 16:01:02 +0500

    Form Handlechange

--- src/components/AdminComponents/Building.jsx
+++ src/components/AdminComponents/Building.jsx
@@ -1,101 +1,121 @@
-import React from 'react'
+import React , { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 function Building(props) {
+    const [formData, setFormData] = useState({
+        street: '',
+        country:''
+      });
+    function handleChange (e){
+        const { name, value } = e.target;
+        console.log(e.target," e.target")
+        console.log(name,"name", value, " value")
+
+        setFormData({
+            ...formData,
+            [name]: value,
+          });
+          console.log(formData,"formData")
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+    }
   return (
     <div>
       <div class="contentCard">
             <div class="titleWrapper"><span>{props.data}</span></div>
             <div class="formDesign">
-                <h3 class="sectionTitle">Address</h3>
-                <div class="row">
-                    <div class="col-12 col-md-4 col-lg-3">
-                        <div class="form-group">
-                            <label>Street Address</label>
-                            <input class="form-control" type="text"/>
+                <form onSubmit={handleSubmit}>
+                    <h3 class="sectionTitle">Address</h3>
+                    <div class="row">
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <div class="form-group">
+                                <label>Street Address</label>
+                                <input class="form-control" type="text" id='street' name='street' onChange={handleChange} value={formData.street}/>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <div class="form-group">
+                                <label>Country</label>
+                                <input class="form-control" type="text" id='country' name='country' onChange={handleChange} value={formData.country}/>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <div class="form-group">
+                                <label>Province</label>
+                                <input class="form-control" type="text"/>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <div class="form-group">
+                                <label>City</label>
+                                <input class="form-control" type="text"/>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <div class="form-group">
+                                <label>Postal Code</label>
+                                <input class="form-control" type="text"/>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <div class="form-group">
+                                <label>Legacy ID</label>
+                                <input class="form-control" type="text"/>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <div class="form-group">
+                                <label>Area Size</label>
+                                <input class="form-control" type="text"/>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <div class="form-group">
+                                <label>Region</label>
+                                <input class="form-control" type="text"/>
+                            </div>
+                        </div>
+                        <div class="col-12  ">
+                            <div class="form-group">
+                                <label>Description</label>
+                                <textarea class="form-control" rows="5"></textarea>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-12 col-md-4 col-lg-3">
-                        <div class="form-group">
-                            <label>Country</label>
-                            <input class="form-control" type="text"/>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4 col-lg-3">
-                        <div class="form-group">
-                            <label>Province</label>
-                            <input class="form-control" type="text"/>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4 col-lg-3">
-                        <div class="form-group">
-                            <label>City</label>
-                            <input class="form-control" type="text"/>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4 col-lg-3">
-                        <div class="form-group">
-                            <label>Postal Code</label>
-                            <input class="form-control" type="text"/>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4 col-lg-3">
-                        <div class="form-group">
-                            <label>Legacy ID</label>
-                            <input class="form-control" type="text"/>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4 col-lg-3">
-                        <div class="form-group">
-                            <label>Area Size</label>
-                            <input class="form-control" type="text"/>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4 col-lg-3">
-                        <div class="form-group">
-                            <label>Region</label>
-                            <input class="form-control" type="text"/>
-                        </div>
-                    </div>
-                    <div class="col-12  ">
-                        <div class="form-group">
-                            <label>Description</label>
-                            <textarea class="form-control" rows="5"></textarea>
-                        </div>
-                    </div>
-                </div>
 
-                <hr class="splitCard"/>
+                    <hr class="splitCard"/>
 
-                <h3 class="sectionTitle">Building Contact (Optional)</h3>
-                <div class="row">
-                    <div class="col-12 col-md-4 col-lg-3">
-                        <div class="form-group">
-                            <label>Full Name</label>
-                            <input class="form-control" type="text"/>
+                    <h3 class="sectionTitle">Building Contact (Optional)</h3>
+                    <div class="row">
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <div class="form-group">
+                                <label>Full Name</label>
+                                <input class="form-control" type="text"/>
+                            </div>
                         </div>
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <div class="form-group">
+                                <label>Contact</label>
+                                <input class="form-control" type="text"/>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input class="form-control" type="text"/>
+                            </div>
+                        </div>
+                        {/* <!-- <div class="col-12 col-md-4 col-lg-3">
+                            <div class="form-group">
+                                <label>PM Email</label>
+                                <input class="form-control" type="text">
+                            </div>
+                        </div> --> */}
                     </div>
-                    <div class="col-12 col-md-4 col-lg-3">
-                        <div class="form-group">
-                            <label>Contact</label>
-                            <input class="form-control" type="text"/>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4 col-lg-3">
-                        <div class="form-group">
-                            <label>Email</label>
-                            <input class="form-control" type="text"/>
-                        </div>
-                    </div>
-                    {/* <!-- <div class="col-12 col-md-4 col-lg-3">
-                        <div class="form-group">
-                            <label>PM Email</label>
-                            <input class="form-control" type="text">
-                        </div>
-                    </div> --> */}
-                </div>
 
-                <a class="myBtn" href="buildingList.html">{(props.data == 'Edit Building')?'Save':'Add'}  Building</a>
-                <a class="myBtn bdr" href="buildingList.html">Cancel</a>
+                    <button class="myBtn" type='submit'>{(props.data == 'Edit Building')?'Save':'Add'}  Building</button>
+                    <a class="myBtn bdr" href="buildingList.html">Cancel</a>
+                </form>
             </div>
         </div>
     </div>
--- src/components/AdminComponents/ManageBuildings.jsx
+++ src/components/AdminComponents/ManageBuildings.jsx
@@ -2,27 +2,35 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios';
 function ManageBuildings() {
-    let buildingsApiString = 'http://localhost:3001/posts'
+    const [BuildingData, setBuildingData] = useState([])  
+    let buildingsApiString = 'http://localhost:3001/buildings'
     useEffect(() => {
         console.log('useEffect ran'); // Add this line
-        // axios.get('http://localhost:3001/posts')
-        //   .then((response) => {
-        //     console.log(response.data)
-        //   })
-        //   .catch((error) => {
-        //     console.error('Error fetching data:', error);
-        //   });
-        axios.post('http://localhost:3001/posts', { "id": 3, "title": "JSON Server Example2" })
-          .then(function (response) {
-            console.log(response);
+        axios.get('http://localhost:3001/buildings')
+          .then((response) => {
+            console.log(response.data)
+            setBuildingData(response.data)
+            console.log(BuildingData,"BuildingData")
           })
-          .catch(function (error) {
-            console.log(error);
+          .catch((error) => {
+            console.error('Error fetching data:', error);
           });
+        // axios.post('http://localhost:3001/posts', { "id": 3, "title": "JSON Server Example2" })
+        //   .then(function (response) {
+        //     console.log(response);
+        //   })
+        //   .catch(function (error) {
+        //     console.log(error);
+        //   });
       }, []);
       console.log('Component rendered'); // Add this line
   return (
     <>
+        {/* <ul>
+            {BuildingData.map((buildingEle, buildingIndex)=>(
+                <li key ={buildingIndex}>{buildingEle.id}:{buildingEle.title}</li>
+            ))} 
+        </ul> */}
         <div className="contentCard">
             <div className="titleWrapper"><span>Manage Buildings</span>
 
@@ -72,50 +80,54 @@ function ManageBuildings() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td><input type="checkbox" className="customCheck"/></td>
-                            <td><b>1</b></td>
-                            <td><img src="https://img.icons8.com/external-flatart-icons-lineal-color-flatarticons/28/000000/external-building-real-estate-flatart-icons-lineal-color-flatarticons-1.png"/> 1030 Thinkbound Drive</td>
-                            <td>Oakville</td>
-                            <td>L6H 2A8</td>
-                            <td>Ontario</td>
-                            <td>Oakville</td>
-                            <td>Trafalgar & QEW</td>
-                            <td>(867) 969-6766</td>
-                            <td>1</td>
-                            <td className="text-right">
-                                <div className="dropleft">
-                                    <button type="button" className="actionEclips dropdown-toggle" data-toggle="dropdown">
-                                        <span></span>
-                                        <span></span>
-                                        <span></span>
-                                    </button>
-                                    <div className="dropdown-menu dropDesign">
-                                        <Link to={'/dashboard/building_edit'}>Edit 
-                                            <span>
-                                                {/* <svg viewBox="0 0 13.635 14.678">
-                                                    <use xlink:href="#edit"></use>
-                                                </svg> */}
-                                            </span>
-                                        </Link>
-                                        <Link to={'/dashboard/building_detail'}>Details
-                                            <span>
-                                                {/* <svg viewBox="0 0 11.563 14.453">
-                                                    <use xlink:href="#file"></use>
-                                                </svg> */}
-                                            </span>
-                                        </Link>
-                                        <a href="#">Contact
-                                            <span>
-                                                {/* <svg viewBox="0 0 20.496 13.835 ">
-                                                    <use xlink:href="#email"></use>
-                                                </svg> */}
-                                            </span>
-                                        </a>
-                                    </div>
+                    {BuildingData.map((buildingEle, buildingIndex)=>(
+                        <tr key ={buildingIndex}>
+                        <td><input type="checkbox" className="customCheck"/></td>
+                        <td><b>{buildingEle.id}</b></td>
+                        <td><img src="https://img.icons8.com/external-flatart-icons-lineal-color-flatarticons/28/000000/external-building-real-estate-flatart-icons-lineal-color-flatarticons-1.png"/> {buildingEle.address.street}</td>
+                        <td>{buildingEle.address.city}</td>
+                        <td>{buildingEle.address.postalCode}</td>
+                        <td>{buildingEle.address.province}</td>
+                        <td>{buildingEle.address.region}</td>
+                        <td>Trafalgar & QEW</td>
+                        <td>{buildingEle.BuildingContact.contact}</td>
+                        <td>1</td>
+                        <td className="text-right">
+                            <div className="dropleft">
+                                <button type="button" className="actionEclips dropdown-toggle" data-toggle="dropdown">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </button>
+                                <div className="dropdown-menu dropDesign">
+                                    <Link to={'/dashboard/building_edit'}>Edit 
+                                        <span>
+                                            {/* <svg viewBox="0 0 13.635 14.678">
+                                                <use xlink:href="#edit"></use>
+                                            </svg> */}
+                                        </span>
+                                    </Link>
+                                    <Link to={'/dashboard/building_detail'}>Details
+                                        <span>
+                                            {/* <svg viewBox="0 0 11.563 14.453">
+                                                <use xlink:href="#file"></use>
+                                            </svg> */}
+                                        </span>
+                                    </Link>
+                                    <a href="#">Contact
+                                        <span>
+                                            {/* <svg viewBox="0 0 20.496 13.835 ">
+                                                <use xlink:href="#email"></use>
+                                            </svg> */}
+                                        </span>
+                                    </a>
                                 </div>
-                            </td>
-                        </tr>
+                            </div>
+                        </td>
+                    </tr>
+                // <li key ={buildingIndex}>{buildingEle.id}:{buildingEle.title}</li>
+            ))}
+                        
                     </tbody>
                 </table>
             </div>
